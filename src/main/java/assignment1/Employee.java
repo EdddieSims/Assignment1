d2da9cce9695393db8241a6eca405317cce31c27
Edmund <edmundsimons69@gmail.com>
2016-03-03 18:48:32 +0200

    Commiting added java class again

--- src/main/java/assignment1/Employee.java
+++ src/main/java/assignment1/Employee.java
@@ -5,4 +5,25 @@ package assignment1;
  */
 public class Employee
 {
+    int empNum;
+    String empName;
+
+    public Employee() {
+    }
+
+    public void setEmpNum(int empNum) {
+        this.empNum = empNum;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
 }
