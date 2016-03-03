85f16f17b546c8194cddd2fea45bf318119575ad
Bulelani Rwexane <rwexaneb@gmail.com>
2016-03-03 19:44:11 +0200

    Commiting all

--- src/main/java/assignment1/Employee.java
+++ src/main/java/assignment1/Employee.java
@@ -5,25 +5,5 @@ package assignment1;
  */
 public class Employee
 {
-    int empNum;
-    String empName;
-
-    public Employee() {
-    }
-
-    public void setEmpNum(int empNum) {
-        this.empNum = empNum;
-    }
-
-    public void setEmpName(String empName) {
-        this.empName = empName;
-    }
-
-    public int getEmpNum() {
-        return empNum;
-    }
-
-    public String getEmpName() {
-        return empName;
-    }
+    String name;
 }
